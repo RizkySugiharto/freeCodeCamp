23a920ec547b89896dab7ed54040aed34f142f33
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-05-13 11:23:10 +0700

    Complete [Timestamp Microservice] project in [Back End Development and APIs] curriculum

---
+++ Back End Development and APIs/Timestamp Microservice/index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use('/public', express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  const regex = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+  let date, unix, utc;
+
+  if (regex.test(req.params.date)) {
+    unix = Date.parse(req.params.date);
+  } else {
+    unix = parseInt(req.params.date);
+  }
+
+  date = new Date(unix);
+  utc = date.toUTCString();
+
+  if (utc === 'Invalid Date') {
+    res.json({
+      error: utc,
+    });
+  } else {
+    res.json({
+      unix,
+      utc 
+    });
+  }
+
+});
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
